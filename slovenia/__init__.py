bc3422371b4d9d847df20e6106ea036b6cef59f4
matjash <32578708+matjash@users.noreply.github.com>
2021-12-19 20:18:29 +0100

    0.1 restart

--- slovenia/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# noinspection PyPep8Naming
-def classFactory(iface):  # pylint: disable=invalid-name
-
-    from .lidar_slovenia import LidarSlovenia
-    return LidarSlovenia(iface)
